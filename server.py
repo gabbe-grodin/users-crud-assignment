d121eab844dcae8f3c575d28c550fb7f79d08a63
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-07-31 23:09:55 -0500

    connected successfully, debugged and some styling

--- server.py
+++ server.py
@@ -1,22 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from user import User
+
 app = Flask(__name__)
+
+app.secret_key = "khsdfughpaiuhgioasdhvihgharoighvoisdhvkshgiahsdivhlskhgoisdhvs43956723]"
 
 @app.route("/")
 def index():
-    users = User.get_all()
-    print(users)
-    return render_template("index.html", users = users)
+    return render_template("index.html")
 
-@app.route('/users', methods=['POST'])
+@app.route('/users/create', methods=['POST'])
 def create_user():
     data = {
         "first_name": request.form['first_name'],
         "last_name": request.form['last_name'],
         "email": request.form['email']
     }
-    User.save(request.form)
-    return redirect('users.html')
+    # print("session!!!!!!!!!!!!!!")
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['email'] = request.form['email']
+    # print("before!!!!!!!!!")
+    User.save(data)
+    # print("after!!!!!!!!!!!!")
+    return redirect("/users")
+
+@app.route('/users')
+def show_users():
+    users = User.get_all(request.form)
+    print(request.form)
+    # print("___________________________")
+    return render_template("users.html", first_name=session['first_name'], last_name=session['last_name'], email=session['email'],users=users)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=8000)
--- user.py
+++ user.py
@@ -10,21 +10,20 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-
     @classmethod
     def save(cls, data):
         query = """
-                INSERT INTO users (first_name, last_name, email)
-                VALUES (%(first_name)s, %(last_name)s, %(email)s);
+                INSERT INTO users (first_name, last_name, email, created_at, updated_at)
+                VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());
                 """
         return connectToMySQL(cls.DB).query_db(query, data)
     
     @classmethod
-    def get_all(cls):
+    def get_all(cls, data):
         query = """
                 SELECT * FROM users;
                 """
-        results = connectToMySQL(cls.DB).query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query,data)
         users = []
         for user in results:
             users.append(cls(user))
