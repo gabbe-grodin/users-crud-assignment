1ab71cd53142a30973612c2fa15d1e853d39a483
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-08-03 08:49:02 -0500

    CRUD complete, fixed bugs, added style, ready for modularization

--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+import pprint
 from mysqlconnection import connectToMySQL
 
 class User:
@@ -10,21 +11,65 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    # ! CREATE
     @classmethod
-    def save(cls, data):
+    def create_user(cls, data):
         query = """
-                INSERT INTO users (first_name, last_name, email, created_at, updated_at)
-                VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());
+                INSERT INTO users (first_name, last_name, email)
+                VALUES (%(first_name)s, %(last_name)s, %(email)s)
                 """
-        return connectToMySQL(cls.DB).query_db(query, data)
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        pprint.pp(result)
+        return result
     
+    # ! READ
     @classmethod
-    def get_all(cls, data):
+    def get_all_users(cls):
         query = """
-                SELECT * FROM users;
+                SELECT * 
+                FROM users
                 """
-        results = connectToMySQL(cls.DB).query_db(query,data)
+        result = connectToMySQL(cls.DB).query_db(query)
+        pprint.pp(result)
         users = []
-        for user in results:
+        for user in result:
             users.append(cls(user))
         return users
+    
+    # ! UPDATE
+    @classmethod
+    def update_user_by_id(cls, data):
+        query = """
+                UPDATE users
+                SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at = NOW()
+                WHERE id = %(id)s;
+                """
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        pprint.pp(result)
+        return result
+
+    # ! ALSO UPDATE... OR READ? JON SAID UPDATE IN THE VID
+    @classmethod
+    def get_one_user_by_id(cls, data):
+        query = """
+                SELECT *
+                FROM users
+                WHERE id = %(id)s
+                """
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        pprint.pp(result)
+        if result:
+            user = cls(result[0])
+            return user
+        else:
+            print("Can't get user.")
+
+    # ! DELETE
+    @classmethod
+    def delete_one_user(cls, id):
+        query = """
+                DELETE FROM users
+                WHERE id = %(id)s
+                """
+        data = {"id": id}
+        return connectToMySQL(cls.DB).query_db(query, data)
